23a93431a87aeddada586d82eeaf2922beceae3a
ogdmerlin <ogdmerlin@gmail.com>
2025-09-24 19:05:47 +0300

    add: Initial commit - Empty Activity project

---
+++ app/src/main/java/com/example/helloworldappv2/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.helloworldappv2
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Find views by their IDs
+        val textView = findViewById<TextView>(R.id.textView)
+        val button = findViewById<Button>(R.id.button)
+
+        // Set click listener for the button
+        button.setOnClickListener {
+            textView.text = "Button was clicked! Hello from Android!"
+        }
+    }
+}
